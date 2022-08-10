4277f7cda171cf17ed19e6f2fb4293e0bc0040d6
Iulia <iulia.stoica99@yahoo.com>
2022-08-10 20:30:30 +0300

    all done, half of task5
    Signed-off-by: Iulia <iulia.stoica99@yahoo.com>

--- src/main/java/com/db/bookstore/controller/UserController.java
+++ src/main/java/com/db/bookstore/controller/UserController.java
@@ -1,12 +1,13 @@
 package com.db.bookstore.controller;
 
+import com.db.bookstore.model.Book;
 import com.db.bookstore.model.User;
 
+import com.db.bookstore.service.BookService;
 import com.db.bookstore.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.Cookie;
@@ -17,21 +18,25 @@ public class UserController {
     @Autowired
     UserService userService;
 
+    @Autowired
+    BookService bookService;
+
     @GetMapping("/register")
-    public ModelAndView getRegisterForm(){
+    public ModelAndView getRegisterForm() {
         ModelAndView modelAndView = new ModelAndView("register-form");
         return modelAndView;
     }
 
     @PostMapping("/register")
-    public ModelAndView addUser(User user){
+    public ModelAndView addUser(User user) {
+        user.setRole("client");
         userService.insertUser(user);
         ModelAndView modelAndView = new ModelAndView("redirect:/login");
         return modelAndView;
     }
 
     @GetMapping("/login")
-    public ModelAndView getLoginForm(){
+    public ModelAndView getLoginForm() {
         ModelAndView modelAndView = new ModelAndView("login-form");
         return modelAndView;
     }
@@ -48,18 +53,37 @@ public class UserController {
         ModelAndView modelAndView = new ModelAndView("redirect:/dashboard");
 
         return modelAndView;
-
-
-
     }
 
     @GetMapping("/dashboard")
-    public ModelAndView getDashBoard(){
+    public ModelAndView getDashBoard(@CookieValue int id) {
+        User user = new User();
+        user.setId(id);
         ModelAndView modelAndView=new ModelAndView("dashboard");
+        try {
+            modelAndView.addObject("user", userService.findById(user));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
         modelAndView.addObject("bookList","");
         return modelAndView;
     }
 
+    @GetMapping("/addbook")
+    public ModelAndView addBook(){
+        Book book = new Book();
+        ModelAndView modelAndView=new ModelAndView("addbook");
+        modelAndView.addObject("book", book);
+        return modelAndView;
+    }
+
+    @PostMapping("/book/th-new")
+    public ModelAndView addThBook (@ModelAttribute Book book) {
+        ModelAndView modelAndView = new ModelAndView("added-book");
+        modelAndView.addObject("book", book);
+        bookService.insertBook(book);
+        return modelAndView;
+    }
 
 
 }
--- src/main/java/com/db/bookstore/service/BookService.java
+++ src/main/java/com/db/bookstore/service/BookService.java
@@ -1,5 +1,17 @@
 package com.db.bookstore.service;
 
+import com.db.bookstore.model.Book;
+import com.db.bookstore.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class BookService {
 
+    @Autowired
+    BookRepository bookRepository;
+
+    public void insertBook(Book book) {
+        bookRepository.save(book);
+    }
 }
